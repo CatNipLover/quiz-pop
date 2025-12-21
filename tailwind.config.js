339ae3c983a88673bd41d0b2c855a74eebf96559
CatNipLover <blastebite@gmail.com>
2025-12-22 00:21:35 +0100

    Naprawienie tailwinda (hope so)

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // To mówi: szukaj we wszystkich folderach w src
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#fff7ed", // Kremowy
+        card: "#ffffff",
+        primary: {
+          DEFAULT: "#8b5cf6", // Fiolet
+          dark: "#7c3aed",
+          light: "#ddd6fe",
+        },
+        success: {
+          DEFAULT: "#58cc02", // Zieleń
+          dark: "#46a302",
+        },
+        error: {
+          DEFAULT: "#ef4444", // Czerwień
+          dark: "#b91c1c",
+        },
+        gray: {
+          border: "#e5e7eb",
+          shadow: "#d1d5db",
+        }
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+      }
+    },
+  },
+  plugins: [],
+};
